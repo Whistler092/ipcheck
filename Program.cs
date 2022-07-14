1e86a0794b90e9d21f1f6ed080bac152247d63f9
Ramiro Andres Bedoya Escobar <iamramiroo@gmail.com>
2022-07-14 11:48:55 -0500

    Add code and docker support

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Check if network is available 
+        if (!NetworkInterface.GetIsNetworkAvailable())
+            Console.WriteLine("No Network Connection");
+        else
+        {
+            Console.WriteLine("Current IP Address: ");
+
+            // Get host entry for current hostname
+            var hostname = Dns.GetHostName();
+            var host = Dns.GetHostEntry(hostname);
+
+            // Iterate over each IP Address and render their values
+            foreach (var address in host.AddressList)
+            {
+                Console.WriteLine($"\t{address}");
+            }
+        }
+    }
+}
